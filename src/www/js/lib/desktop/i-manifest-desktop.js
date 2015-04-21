5401b90726a0ad8a347f7c851a7f89552e06d99a
StefanFriedli <friedlistephan@gmail.com>
2015-04-21 20:14:37 +0200

    manifest styling

--- src/www/js/lib/desktop/i-manifest-desktop.js
+++ src/www/js/lib/desktop/i-manifest-desktop.js
@@ -3,6 +3,8 @@ Util.Objects["manifest"] = new function() {
 
 		scene.resized = function() {
 //			u.bug("scene.resized:" + u.nodeId(this));
+			u.as(this, "height", page.browser_h + "px");
+
 		}
 
 		scene.scrolled = function() {
@@ -11,7 +13,8 @@ Util.Objects["manifest"] = new function() {
 
 		scene.ready = function() {
 //			u.bug("scene.ready:" + u.nodeId(this));
-
+			
+			page.resized();
 			
 		}
 
