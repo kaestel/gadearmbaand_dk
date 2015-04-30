a4dfa59a78f839f7618cf81489627357ea9a9192
kaestel <martin@think.dk>
2015-04-30 14:45:46 +0200

    Updated nav names

--- src/templates/www.footer.php
+++ src/templates/www.footer.php
@@ -2,8 +2,8 @@
 
 	<div id="navigation">
 		<ul class="navigation">
-			<li class="front"><h4><a href="/">Frontpage</a></h4></li>
-			<li class="events"><h4><a href="/events">Events</a></h4></li>
+			<li class="front"><h4><a href="/">#gadearmbaand</a></h4></li>
+			<li class="events"><h4><a href="/events">Program</a></h4></li>
 			<li class="manifest"><h4><a href="/manifest">Manifest</a></h4></li>
 			<li class="buy"><h4><a href="http://gosomewhereelse.com" target="_blank">Køb armbånd</a></h4></li>
 		</ul>
